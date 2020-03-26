ef54e9549d7b48842d0f7c4a4a172197676ff216
VladiVK <kozobon@gmail.com>
2020-03-26 14:27:38 +0200

    created class-service SwapiService which works with network

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Code wich works with network
+// should be better put in separate class-service
+class SwapiService {
+  
+    _apiBase = `https://swapi.co/api`;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    async getResources(url) {
+      const resolve = await fetch(`${this._apiBase}${url}` );
+      if( !resolve.ok ) throw new Error(`Could not fetch ${url}, received ${resolve.status}`)
+      const response = await resolve.json();
+      return response;
+    }
+    // People
+    async getAllPeople() {
+      const res = await this.getResources(`/people/`);
+      return res.results;
+    }
+    getPerson(id) {
+      return this.getResources(`/people/${id}`)
+    }
+    // Planets
+    async getAllPlanets() {
+      const res = await this.getResources(`/planets/`);
+      return res.results;
+    }
+    getPlanet(id) {
+      return this.getResources(`/planets/${id}`);
+    }
+    // Starships
+    async getAllStarships() {
+      const res = await this.getResources(`/starships/`);
+      return res.results;
+    }
+    getStarship(id) {
+      return this.getResources(`/starships/${id}`);
+    }
+}
+
+const swapi = new SwapiService();
+
+
+
+
