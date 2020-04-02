8ceaab2c60c541d1a0ab03f789a63e355a2ab75d
VladiVK <kozobon@gmail.com>
2020-04-02 16:20:36 +0300

    insignificant refactoring

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Spinner = () => {
     return (
       <div>
-            
             <div className='spinner-grow text-warning'></div>
             <div className='spinner-border text-light'></div>
             <div className='spinner-grow text-warning'></div>
